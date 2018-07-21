ba2d1c1dee1fe1591ecae642e990c73cc5d7681e
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-21 03:17:42 +0000

    initial commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         '''
             Return the dictionary
         '''
@@ -53,3 +53,11 @@ class FileStorage:
                 FileStorage.__objects[key] = class_name(**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        '''
+        Deletes an obj
+        '''
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        del FileStorage.__objects[key]
+        self.save()
