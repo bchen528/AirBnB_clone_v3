d18b6691016c1b39c9ff6dddf5a35258f3dfd8ab
MelissaN <majajachiu@yahoo.com>
2018-07-22 05:33:57 +0000

    for relationships, delete all related aspect of obj

--- models/state.py
+++ models/state.py
@@ -15,6 +15,7 @@ class State(BaseModel, Base):
     if os.environ.get("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         name = ""
