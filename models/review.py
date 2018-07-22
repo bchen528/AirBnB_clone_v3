c84144fb9ed194e87e2c5af7da7f28abadd96a13
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-22 18:28:30 +0000

    imported Base variable

--- models/review.py
+++ models/review.py
@@ -5,18 +5,19 @@
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     '''
         Implementation for the Review.
     '''
     __tablename__ = "reviews"
-    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    place_id = ""
-    user_id = ""
-    text = ""
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
