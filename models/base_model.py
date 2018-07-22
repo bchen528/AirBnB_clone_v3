5e636468de3e4a9890044af29ea085e3509d0c77
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-22 01:03:57 +0000

    added delete functions and imported sql alchemy

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,20 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
 class BaseModel:
     '''
         Base class for other classes to be used for the duration.
+    if HBNB_TYPE_STORAGE is "db":
     '''
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(default=datetime.utcnow(), nullable=False)
+    updated_at = Column(default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         '''
             Initialize public instance attributes.
@@ -19,7 +27,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
@@ -48,6 +55,7 @@ class BaseModel:
             Update the updated_at attribute with new.
         '''
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -58,5 +66,9 @@ class BaseModel:
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-
+        if hasattr(self, "_sa_instance_state"):
+            del cp_dct["_sa_instance_state"]
         return (cp_dct)
+
+    def delete(self):
+        models.storage.delete(self)
