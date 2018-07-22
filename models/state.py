4f05608649365bb4d1fa68c83e607d97e9b1d198
MelissaN <majajachiu@yahoo.com>
2018-07-22 06:54:25 +0000

    add FileStorage relationship between State and City classes

--- models/state.py
+++ models/state.py
@@ -11,6 +11,7 @@ from models.base_model import BaseModel, Base
 class State(BaseModel, Base):
     '''
         Implementation for the State.
+        Create relationship between class State (parent) to City (child)
     '''
     if os.environ.get("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
@@ -19,3 +20,16 @@ class State(BaseModel, Base):
                               cascade="all, delete, delete-orphan")
     else:
         name = ""
+
+    if os.environ.get("HBNB_TYPE_STORAGE") == "fs":
+        @property
+        def cities:
+            '''
+                Return list of city instances if City.state_id==current State.id
+                FileStorage relationship between State and City
+            '''
+            list_cities = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
