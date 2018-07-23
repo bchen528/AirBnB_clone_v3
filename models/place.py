7c261281d2042f502aa0748d5358e7d29a0a6cce
MelissaN <majajachiu@yahoo.com>
2018-07-23 16:34:43 +0000

    add to place class db relationship w/ review and fs getter

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@
 '''
 from os import getenv
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -15,7 +16,7 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        name_id = Column(String(128), nullable=False)
+        name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, default=0, nullable=False)
         number_bathrooms = Column(Integer, default=0, nullable=False)
@@ -23,6 +24,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place",
+                               cascade="all, delete, delete-orphan")
     else:
         city_id = ""
         user_id = ""
@@ -35,3 +38,16 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "fs":
+        @property
+        def reviews(self):
+            '''
+                Return list: review instances if Review.place_id==curr place.id
+                FileStorage relationship between Place and Review
+            '''
+            list_reviews = []
+            for review in models.storage.all(Review).values():
+                if review.place_id == self.id:
+                    list_reviews.append(review)
+            return list_reviews
