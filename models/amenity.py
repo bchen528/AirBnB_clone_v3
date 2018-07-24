7334e12140ab64b817ee7d0f9d602ac168085b9c
MelissaN <majajachiu@yahoo.com>
2018-07-24 00:12:27 +0000

    add to amenity class relationship with place class

--- models/amenity.py
+++ models/amenity.py
@@ -16,6 +16,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
     else:
         name = ""
