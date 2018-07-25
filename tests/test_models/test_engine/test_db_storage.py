0c3e4d3ec7d7860104e2ab05c32248e1069dcecf
adrielt07 <adriel_tolentino@outlook.com>
2018-07-25 08:00:46 +0000

    initial commit

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''
+    Testing the file_storage module.
+'''
+import time
+import unittest
+from models.engine.db_storage import DBStorage
+from models import *
+from os import getenv
+
+db = getenv("HBNB_TYPE_STORAGE")
+
+
+@unittest.skipIf(db != 'db', "Testing DBstorage only")
+class test_DBStorage(unittest.TestCase):
+    '''
+        Testing the DB_Storage class
+    '''
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Initializing classes
+        '''
+        cls.storage = DBStorage()
+
+    @classmethod
+    def tearDownClass(cls):
+        '''
+            delete variables
+        '''
+        del cls.storage
+
+    def test_new(self):
+        '''
+            Test DB new
+        '''
+        new_obj = State(name="California")
+        self.assertEqual(new_obj.name, "California")
+
+    def test_dbstorage_user_attr(self):
+        '''
+            Testing User attributes
+        '''
+        new = User(email="melissa@hbtn.com", password="hello")
+        self.assertTrue(new.email, "melissa@hbtn.com")
