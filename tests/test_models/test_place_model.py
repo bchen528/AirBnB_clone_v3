e60f8d53f05e3e371045604f1312e9822cd452a9
adrielt07 <adriel_tolentino@outlook.com>
2018-07-25 07:50:18 +0000

    remove pep8 test

--- tests/test_models/test_place_model.py
+++ tests/test_models/test_place_model.py
@@ -36,14 +36,6 @@ class TestUser(unittest.TestCase):
         except FileNotFoundError:
             pass
 
-#    def test_pep8_style_check(self):
-#        '''
-#            Tests pep8 style
-#        '''
-#        style = pep8.StyleGuide(quiet=True)
-#        p = style.check_files(['models/places.py'])
-#        self.assertEqual(p.total_errors, 0, "pep8 error needs fixing")
-#
     def test_Place_dbtable(self):
         '''
             Check if the tablename is correct
