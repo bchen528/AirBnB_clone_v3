f4df19acf7dc1f9185192e0baf075f47885ac96c
adrielt07 <adriel_tolentino@outlook.com>
2018-07-26 02:22:09 +0000

    Added test for all method

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,10 +4,14 @@
 '''
 import time
 import unittest
+import sys
 from models.engine.db_storage import DBStorage
 from models.user import User
 from models.state import State
+from models import storage
+from console import HBNBCommand
 from os import getenv
+from io import StringIO
 
 db = getenv("HBNB_TYPE_STORAGE")
 
@@ -22,14 +26,21 @@ class test_DBStorage(unittest.TestCase):
         '''
             Initializing classes
         '''
-        cls.storage = DBStorage()
-
+        cls.dbstorage = DBStorage()
+        cls.output = StringIO()
+        sys.stdout = cls.output
     @classmethod
     def tearDownClass(cls):
         '''
             delete variables
         '''
-        del cls.storage
+        del cls.dbstorage
+
+    def create(self):
+        '''
+            Create HBNBCommand()
+        '''
+        return HBNBCommand()
 
     def test_new(self):
         '''
@@ -44,3 +55,38 @@ class test_DBStorage(unittest.TestCase):
         '''
         new = User(email="melissa@hbtn.com", password="hello")
         self.assertTrue(new.email, "melissa@hbtn.com")
+
+    def test_dbstorage_check_method(self):
+        '''
+            Check methods exists
+        '''
+        self.assertTrue(hasattr(self.dbstorage, "all"))
+        self.assertTrue(hasattr(self.dbstorage, "__init__"))
+        self.assertTrue(hasattr(self.dbstorage, "new"))
+        self.assertTrue(hasattr(self.dbstorage, "save"))
+        self.assertTrue(hasattr(self.dbstorage, "delete"))
+        self.assertTrue(hasattr(self.dbstorage, "reload"))
+
+    def test_dbstorage_all(self):
+        '''
+            Testing all function
+        '''
+        storage.reload()
+        result = storage.all("")
+        self.assertIsInstance(result, dict)
+        self.assertEqual(len(result), 0)
+        new = User(email="adriel@hbtn.com", password="abc")
+        console = self.create()
+        console.onecmd("create State name=California")
+        result = storage.all("State")
+        self.assertTrue(len(result) > 0)
+
+"""
+        x = (self.output.getvalue())
+        print(x)
+
+        console.onecmd("create User email=adriel@hbnb.com password=abc")
+        self.storage.save()
+        result = self.storage.all("User")
+        print(result)5A
+"""
