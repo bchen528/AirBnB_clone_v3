ed1ea05ffc6800889d1d5f37571ad15f4c6f075e
MelissaN <majajachiu@yahoo.com>
2018-07-30 22:43:12 +0000

    curl didn't work; add directory preslashes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,7 @@
 # Recursively assign ownership of /data/ folder to user and group 'ubuntu'
 # Update the Nginx config to serve content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static)
 #     restart Nginx
+# curl localhost/hbnb_static/index.html should return sample text"
 
 ADD_WEBSTATIC="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 
@@ -17,8 +18,8 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p data/web_static/releases/test data/web_static/shared
-echo "Test index.html file to test Nginx config" | sudo tee data/web_static/releases/test/index.html
-sudo ln -sf data/web_static/releases/test data/web_static/current
+echo "Test index.html file to test Nginx config" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu data/
 sudo sed -i "35i $ADD_WEBSTATIC" /etc/nginx/sites-available/default
 sudo service nginx start
