4db87fb64e0e4f8760fd0d17bfcdd571d1d7e487
MelissaN <majajachiu@yahoo.com>
2018-07-31 00:04:13 +0000

    fix pep8 style; add .tgz extension

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Fabric script generates .tgz archive of all in web_static/ using function 'do_pack'
+Fabric script generates .tgz archive of all in web_static/ using func 'do_pack'
 Usage: fab -f 1-pack_web_static.py do_pack
 
 All files in the folder web_static must be added to the final archive
-All archives must be stored in the folder versions (your function should create this folder if it doesn’t exist)
-The name of the archive created must be web_static_<year><month><day><hour><minute><second>.tgz
-The function do_pack must return the archive path if the archive has been correctly generated. Otherwise, it should return None
+All archives must be stored in the folder 'versions' (create folder if none)
+Create archive "web_static_<year><month><day><hour><minute><second>.tgz"
+The function do_pack must return the archive path, else return None
 """
 from fabric.api import local
 from time import strftime
@@ -17,7 +17,7 @@ def do_pack():
     timenow = strftime("%Y%M%d%H%M%S")
     try:
         local("mkdir -p versions")
-        filename = "versions/web_static_{}".format(timenow)
+        filename = "versions/web_static_{}.tgz".format(timenow)
         local("tar -cvzf {} web_static/".format(filename))
         return filename
     except:
