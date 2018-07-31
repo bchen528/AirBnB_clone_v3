8ddea37a82474de30fe85638524780fd6c27d3be
MelissaN <majajachiu@yahoo.com>
2018-07-31 03:00:03 +0000

    filename typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,7 +21,7 @@ def do_deploy(archive_path):
     try:
         filename = archive_path.split("/")[-1]
         no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web-static/releases/{}/".format(no_ext)
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
         symlink = "/data/web_static/current"
         put(archive_path, "/tmp/")
         run("mkdir -p {}".format(path_no_ext))
