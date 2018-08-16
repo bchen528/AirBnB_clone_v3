4226baf89b436b1a8c630c17e6a856eecd923eee
MelissaN <majajachiu@yahoo.com>
2018-08-16 05:42:20 +0000

    display properties and matching owner names via special var coding

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -137,10 +137,18 @@ def html_all_filters():
     state_objs = [s for s in storage.all("State").values()]
     amenity_objs = [a for a in storage.all("Amenity").values()]
     place_objs = [p for p in storage.all("Place").values()]
+    user_objs = [u for u in storage.all("User").values()]
+    place_owner_objs = []
+    for place in place_objs:
+        for user in user_objs:
+            if place.user_id == user.id:
+                place_owner_objs.append(["{} {}".format(
+                    user.first_name, user.last_name), place])
+    place_owner_objs.sort(key=lambda p: p[1].name)
     return render_template('100-hbnb.html',
                            state_objs=state_objs,
                            amenity_objs=amenity_objs,
-                           place_objs=place_objs)
+                           place_owner_objs=place_owner_objs)
 
 
 if __name__ == "__main__":
