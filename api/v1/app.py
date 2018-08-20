be0d5fc8fa2a9cceb7260ccf7a27eb9aad06c8a0
Alex Allen <325@holbertonschool.com>
2018-08-20 21:50:37 +0000

    add task 6 not found 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def tear(self):
     ''' closes storage engine '''
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__ == '__main__':
     if getenv("HBNB_API_HOST") is None:
