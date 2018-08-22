dc9011065ca7c6d57ff3002eb73c05df7e924e35
Alex Allen <325@holbertonschool.com>
2018-08-22 17:30:44 +0000

    reinsert state_id for ORM, add strict_slashes is false

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,6 +10,7 @@ import uuid
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
+@app_views.route('/states/<state_id>/cities/', methods=['GET'])
 def list_cities_of_state(state_id):
     '''Retrieves a list of all City objects'''
     all_states = storage.all("State").values()
@@ -22,6 +23,7 @@ def list_cities_of_state(state_id):
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
+@app_views.route('/states/<state_id>/cities/', methods=['POST'])
 def create_city(state_id):
     '''Creates a City'''
     if not request.get_json():
@@ -33,7 +35,7 @@ def create_city(state_id):
     if state_obj == []:
         abort(404)
     cities = []
-    new_city = City(name=request.json['name'])
+    new_city = City(name=request.json['name'], state_id=state_id)
     storage.new(new_city)
     storage.save()
     cities.append(new_city.to_dict())
