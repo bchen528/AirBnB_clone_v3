ee010430c36073d0fc48a191c646d928a3c34a6b
bchen528 <bchen720@gmail.com>
2018-08-24 02:08:26 +0000

    add tests for delete, post, put, get

--- tests/test_api/test_v1/test_views/test_states.py
+++ tests/test_api/test_v1/test_views/test_states.py
@@ -13,7 +13,7 @@ from models import storage
 
 class TestState(unittest.TestCase):
     '''test state'''
-    def test_lists_states(self):        
+    def test_lists_states(self):
         '''test state GET route'''
         with app.test_client() as c:
             resp = c.get('/api/v1/states')
@@ -21,10 +21,12 @@ class TestState(unittest.TestCase):
             resp2 = c.get('/api/v1/states/')
             self.assertEqual(resp.status_code, 200)
 
-    def test_create_state(self):        
+    def test_create_state(self):
         '''test state POST route'''
         with app.test_client() as c:
-            resp = c.post('/api/v1/states/', data=json.dumps({"name": "California"}), content_type="application/json")
+            resp = c.post('/api/v1/states/',
+                          data=json.dumps({"name": "California"}),
+                          content_type="application/json")
             self.assertEqual(resp.status_code, 201)
 
     def test_delete_state(self):
@@ -35,36 +37,30 @@ class TestState(unittest.TestCase):
             resp = c.get('api/v1/states/{}'.format(new_state.id))
             self.assertEqual(resp.status_code, 200)
             resp1 = c.delete('api/v1/states/{}'.format(new_state.id))
-            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp1.status_code, 404)
             resp2 = c.get('api/v1/states/{}'.format(new_state.id))
-            self.assertEqual(resp.status_code, 200)
-            # last line should return 404 status code
-            
-"""
-    def test_get_state(self):        
+            self.assertEqual(resp2.status_code, 404)
+
+    def test_get_state(self):
         '''test state GET by id route'''
         with app.test_client() as c:
-            resp = c.get('/api/v1/states')
-            data = json.loads(resp.data.decode('utf-8'))
-            self.assertEqual(data, {'status': 'OK'})
+            new_state = State(name="Beckystan")
+            storage.new(new_state)
+            resp = c.get('api/v1/states/{}'.format(new_state.id))
+            self.assertEqual(resp.status_code, 200)
 
-
-    def test_delete_state(self):        
-        '''test state DELETE route'''
-        with app.test_client() as c:
-            resp = c.delete('/api/v1/states/<state_id>')
-            data = json.loads(resp.data.decode('utf-8'))
-            self.assertEqual(data, {'status': 'OK'})
-"""
-
-"""
-    def test_update_state(self):        
+    def test_update_state(self):
         '''test state PUT route'''
         with app.test_client() as c:
-            resp = c.get('/api/v1/states')
-            data = json.loads(resp.data.decode('utf-8'))
-            self.assertEqual(data, {'status': 'OK'})
-"""
+            new_state = State(name="Beckystan")
+            storage.new(new_state)
+            resp = c.put('api/v1/states/{}'.format(new_state.id),
+                         data=json.dumps({"name": "Beckytopia"}),
+                         content_type="application/json")
+            # data = json.loads(resp.data.decode('utf-8'))
+            # print(data)
+            self.assertEqual(resp.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
